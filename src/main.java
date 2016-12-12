0d8ab89d58bbff7092becfe4b3041feb51599c0b
Seaiya Takusi <e165737@ie.u-ryukyu.ac.jp>
2016-12-12 21:49:13 +0900

    report souse code

---
+++ src/main.java
@@ -0,0 +1,23 @@
+/**
+ * Created by e165737 on 2016/12/12.
+ */
+public class main {
+    public static void main(String[] args){
+        String str = "null";
+
+        try {
+            System.out.println("文字列の長さ:"+str.length());
+            Double.parseDouble(str);
+        }
+        catch (NullPointerException e) {
+            System.out.println("エラー:" + e.toString() + "が発生プログラムを終了。");
+            e.printStackTrace();
+        }
+        catch (NumberFormatException e){
+            System.out.println("エラー:" + e.toString() +"が発生プログラムを終了。" );
+            e.printStackTrace();
+        }
+    }
+
+}
+
